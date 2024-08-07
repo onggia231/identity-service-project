9f4ebf09584ef02114e2d27713182eb12120ccc1
NgocBn <ngocbn@telsoft.com.vn>
2024-08-07 14:16:25 +0700

    Package Constant

---
+++ src/main/java/com/onggia/identityserviceproject/constant/PredefinedRole.java
@@ -0,0 +1,8 @@
+package com.onggia.identityserviceproject.constant;
+
+public class PredefinedRole {
+    public static final String USER_ROLE = "USER";
+    public static final String ADMIN_ROLE = "ADMIN";
+
+    private PredefinedRole() {}
+}

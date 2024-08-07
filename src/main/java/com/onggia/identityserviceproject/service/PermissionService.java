cd8d387ea1b26ee77fa024e7f43a783aca827fe5
NgocBn <ngocbn@telsoft.com.vn>
2024-08-07 14:41:00 +0700

    Package Service

---
+++ src/main/java/com/onggia/identityserviceproject/service/PermissionService.java
@@ -0,0 +1,38 @@
+package com.onggia.identityserviceproject.service;
+
+import com.onggia.identityserviceproject.dto.request.PermissionRequest;
+import com.onggia.identityserviceproject.dto.response.PermissionResponse;
+import com.onggia.identityserviceproject.entity.Permission;
+import com.onggia.identityserviceproject.mapper.PermissionMapper;
+import com.onggia.identityserviceproject.repository.PermissionRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class PermissionService {
+    PermissionRepository permissionRepository;
+    PermissionMapper permissionMapper;
+
+    public PermissionResponse create(PermissionRequest request) {
+        Permission permission = permissionMapper.toPermission(request);
+        permission = permissionRepository.save(permission);
+        return permissionMapper.toPermissionResponse(permission);
+    }
+
+    public List<PermissionResponse> getAll() {
+        var permissions = permissionRepository.findAll();
+        return permissions.stream().map(permissionMapper::toPermissionResponse).toList();
+    }
+
+    public void delete(String permission) {
+        permissionRepository.deleteById(permission);
+    }
+}
---
+++ src/main/java/com/onggia/identityserviceproject/service/RoleService.java
@@ -0,0 +1,45 @@
+package com.onggia.identityserviceproject.service;
+
+import com.onggia.identityserviceproject.dto.request.RoleRequest;
+import com.onggia.identityserviceproject.dto.response.RoleResponse;
+import com.onggia.identityserviceproject.mapper.RoleMapper;
+import com.onggia.identityserviceproject.repository.PermissionRepository;
+import com.onggia.identityserviceproject.repository.RoleRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class RoleService {
+
+    RoleRepository roleRepository;
+    PermissionRepository permissionRepository;
+    RoleMapper roleMapper;
+
+    public RoleResponse create(RoleRequest request) {
+        var role = roleMapper.toRole(request);
+
+        var permissions = permissionRepository.findAllById(request.getPermissions());
+        role.setPermissions(new HashSet<>(permissions));
+
+        role = roleRepository.save(role);
+        return roleMapper.toRoleResponse(role);
+    }
+
+    public List<RoleResponse> getAll() {
+        var roles = roleRepository.findAll();
+        return roles.stream().map(roleMapper::toRoleResponse).toList();
+    }
+
+    public void delete(String role) {
+        roleRepository.deleteById(role);
+    }
+}

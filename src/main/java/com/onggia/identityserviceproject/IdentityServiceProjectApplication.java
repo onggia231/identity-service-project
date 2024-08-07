80a246c034368c9801d26796b029ee8c9f59a3d4
NgocBn <ngocbn@telsoft.com.vn>
2024-08-07 11:35:52 +0700

    First commit

---
+++ src/main/java/com/onggia/identityserviceproject/IdentityServiceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.onggia.identityserviceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityServiceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdentityServiceProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onggia/identityserviceproject/IdentityServiceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onggia.identityserviceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityServiceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

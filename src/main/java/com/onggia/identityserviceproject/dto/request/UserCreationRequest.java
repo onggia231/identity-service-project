bdb7fc62f8de76f7ebc3f2d255382f544b66206e
NgocBn <ngocbn@telsoft.com.vn>
2024-08-07 14:15:04 +0700

    Package Dto(request, response)

---
+++ src/main/java/com/onggia/identityserviceproject/dto/request/UserCreationRequest.java
@@ -0,0 +1,29 @@
+package com.onggia.identityserviceproject.dto.request;
+
+import com.onggia.identityserviceproject.validator.DobConstraint;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserCreationRequest {
+    @Size(min = 4, message = "USERNAME_INVALID")
+    String username;
+
+    @Size(min = 6, message = "INVALID_PASSWORD")
+    String password;
+
+    String firstName;
+    String lastName;
+
+    @DobConstraint(
+            min = 18,
+            message = "INVALID_DOB") // DobConstraint custom, INVALID_DOB duoc mo ta ma loi o class ErrorCode
+    LocalDate dob;
+}
